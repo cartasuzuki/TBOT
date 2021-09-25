9806feefd0c11d14968fee26128cfcde96a6534c
cartasuzuki <46107174+cartasuzuki@users.noreply.github.com>
2021-09-26 06:42:12 +0900

    Add files via upload

--- inference.py
+++ inference.py
@@ -1,16 +1,19 @@
 """Sample prediction script for TensorFlow 2"""
 import argparse
+from pandas.core.series import Series
 import tensorflow as tf
 import numpy as np
 import PIL.Image, PIL.ImageDraw
+import tbotcam
 from matplotlib import pyplot as plt
 import matplotlib.image as mpimg
+import pandas as pd
 from cv2 import *
 
 
 MODEL_FILENAME = 'model.pb'
 LABELS_FILENAME = 'labels.txt'
-IMAGE_FILENAME = 'Tea-Leaves-Growing.jpg'
+IMAGE_FILENAME = 'Tea-Leaves-Growing2.jpg'
 WEBCAM_IMAGE_FILENAME = 'webcam.jpg'
 
 class ObjectDetection:
@@ -39,14 +42,6 @@ class ObjectDetection:
             output_tensors = [sess.graph.get_tensor_by_name(n) for n in self.OUTPUT_TENSOR_NAMES]
             outputs = sess.run(output_tensors, {self.INPUT_TENSOR_NAME: inputs})
             return outputs
-
-
-def convert_to_opencv(image):
-    # RGB -> BGR conversion is performed as well.
-    image = image.convert('RGB')
-    r,g,b = np.array(image).T
-    opencv_image = np.array([b,g,r]).transpose()
-    return opencv_image
 
 
 def take_photo_from_webcam():
@@ -120,20 +115,74 @@ def PrintPredictions(predictions, labels_filename):
     for pred in zip(*predictions):
         print(f"Class: {labels[pred[2]]}, Probability: {pred[1]}, Bounding box: {pred[0]}")
 
+def CreatePredictionDataFrame(predictions, imagefilename):
+    predictionsDF = pd.DataFrame(columns = ["x", "y", "X", "Y"])
+    image = PIL.Image.open(imagefilename)
+    w, h = image.size
+    for pred in zip(*predictions):
+        if(pred[1]>0.3):
+            predSeries = pd.Series(pred[0], index=predictionsDF.columns)
+            predSeries['x'] = predSeries['x'] * w
+            predSeries['X'] = predSeries['X'] * w
+            predSeries['y'] = predSeries['y'] * h
+            predSeries['Y'] = predSeries['Y'] * h
+            print("PredSERIES")
+            print(predSeries)
+            predictionsDF = predictionsDF.append(predSeries,ignore_index=True)
+    return predictionsDF
+
+def ConvertPredictionsToRealWorldCoordinates(predictionsDF):
+    
+    
+    cam = tbotcam.cam
+    cam.LoadFromFile(cam)
+    data = []
+    for i in range(len(predictionsDF)) :
+        pred = predictionsDF.iloc[i]
+        print(pred)
+        xyz = cam.calculate_XYZ(cam, pred['x'],pred['y'] )
+        XYZ = cam.calculate_XYZ(cam, pred['X'],pred['Y'] )
+        item = np.append(xyz,XYZ)
+        data.append(item)
+
+    predictionsRealWorldCoordinatesDF = pd.DataFrame(data, columns = ["x", "y", "z", "X", "Y", "Z"])    
+
+    return predictionsRealWorldCoordinatesDF
+
+
+def test_webcam(mirror=False):
+    cam = cv2.VideoCapture(0)
+    while True:
+        ret_val, img = cam.read()
+        if mirror: 
+            img = cv2.flip(img, 2)
+        cv2.imshow('my webcam', img)
+        if cv2.waitKey(1) == 27: 
+            break  # esc to quit
+    cv2.destroyAllWindows()
 
 def main():
     parser = argparse.ArgumentParser('Object Detection for Custom Vision TensorFlow model')
-    parser.add_argument('--image_filename', type=str, default= IMAGE_FILENAME, help='Filename for the input image')
+    parser.add_argument('--image_filename', type=str, default= WEBCAM_IMAGE_FILENAME, help='Filename for the input image')
     parser.add_argument('--model_filename', type=str, default=MODEL_FILENAME, help='Filename for the tensorflow model')
     parser.add_argument('--labels_filename', type=str, default=LABELS_FILENAME, help='Filename for the labels file')
     args = parser.parse_args()
     
+  #  test_webcam()
+
     #webcam_image_filename = take_photo_from_webcam()
     predictions = predict(args.model_filename, args.image_filename)
-
+    
     PrintPredictions(predictions, args.labels_filename)    
 
     ShowImageWithBoundingBoxes(args.image_filename, predictions)
+
+    predictionsDF = CreatePredictionDataFrame(predictions, WEBCAM_IMAGE_FILENAME)
+    realworld = ConvertPredictionsToRealWorldCoordinates(predictionsDF)
+    print("Real World Coordinates")
+    print(realworld)
+    print(predictionsDF)
+    print("Finished...")
 
 if __name__ == '__main__':
     main()
